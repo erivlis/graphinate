ce635478f5dc35a71da5697d60215419b8cc594b
Eran Rivlis <eran.rivlis@yahoo.com>
2023-07-09 21:10:54 +0300

    add github followers example

---
+++ examples/github/followers_graph.py
@@ -0,0 +1,33 @@
+import graphinate
+from graphinate.graphs.networkx import NetworkxGraphType
+from graphinate.plot import show
+from helpers import _user
+
+DEPTH = 0
+
+graph_model = graphinate.GraphModel(name='github-followers')
+
+
+def _followers(user_id: str | None = None, depth: int = 0, **kwargs):
+    user = _user(user_id)
+    for follower in user.get_followers():
+        yield {'source': user.login, 'target': follower.login}
+        if depth < DEPTH:
+            yield from _followers(follower.login, depth=depth + 1, **kwargs)
+
+
+@graph_model.edge()
+def followed_by(user_id: str | None = None, **kwargs):
+    yield from _followers(user_id, **kwargs)
+
+
+if __name__ == '__main__':
+    networkx_graph = graphinate.graphs.NetworkxGraph(graph_model, graph_type=NetworkxGraphType.Graph)
+
+    params = {
+        # 'user_id': 'erivlis',
+        # 'user_id': 'andybrewer',
+        # 'user_id' "strawberry-graphql"
+    }
+    graph = networkx_graph.build(**params)
+    show(graph)
