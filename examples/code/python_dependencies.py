bf736b68fd95769150e37d8b1bf5be24c0883ca3
Eran Rivlis <eran.rivlis@yahoo.com>
2024-02-10 22:33:27 +0200

    update examples

--- examples/code/python_dependencies.py
+++ examples/code/python_dependencies.py
@@ -28,5 +28,5 @@ if __name__ == '__main__':
     graphinate.materialize(
         dependency_model,
         builder=graphinate.builders.GraphQLBuilder,
-        actualizer=graphinate.plot
+        actualizer=graphinate.graphql
     )
