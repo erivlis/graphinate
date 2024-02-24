a5c77ed157efa58ca8ffff65436a49cb8a1f365b
Eran Rivlis <eran.rivlis@yahoo.com>
2024-02-24 10:25:48 +0200

    update dependencies + misc improvements

--- src/graphinate/server/__init__.py
+++ src/graphinate/server/__init__.py
@@ -3,12 +3,70 @@ import webbrowser
 
 import strawberry
 from starlette.applications import Starlette
+from starlette.schemas import SchemaGenerator
 from strawberry.asgi import GraphQL
 from strawberry.extensions.tracing import OpenTelemetryExtension
 
 from .starlette import routes
 
 DEFAULT_PORT: int = 8072
+
+
+def openapi_schema(request):
+    schema = SchemaGenerator(
+        {
+            "openapi": "3.0.0",
+            "info": {"title": "Graphinate API", "version": "1.0"},
+            "paths": {
+                "/graphql": {
+                    "get": {
+                        "responses": {
+                            200: {
+                                "description": "GraphQL"
+                            }
+                        }
+                    }
+                },
+                "/graphiql": {
+                    "get": {
+                        "responses": {
+                            200: {
+                                "description": "GraphiQL UI."
+                            }
+                        }
+                    }
+                },
+                "/metrics": {
+                    "get": {
+                        "responses": {
+                            200: {
+                                "description": "Prometheus metrics."
+                            }
+                        }
+                    }
+                },
+                "/viewer": {
+                    "get": {
+                        "responses": {
+                            200: {
+                                "description": "3D Force-Directed Graph Viewer"
+                            }
+                        }
+                    }
+                },
+                "/voyager": {
+                    "get": {
+                        "responses": {
+                            200: {
+                                "description": "Voyager GraphQL Schema Viewer"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+    return schema.OpenAPIResponse(request=request)
 
 
 def graphql(graphql_schema: strawberry.Schema, port: int = DEFAULT_PORT):
@@ -43,6 +101,8 @@ def graphql(graphql_schema: strawberry.Schema, port: int = DEFAULT_PORT):
     from starlette_prometheus import PrometheusMiddleware, metrics
     app.add_middleware(PrometheusMiddleware)
     app.add_route("/metrics", metrics)
+
+    app.add_route("/schema", route=openapi_schema, include_in_schema=False)
 
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=port)
