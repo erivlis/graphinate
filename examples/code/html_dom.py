7df30f4b12da9092ec7de55215754ef68ec0a8d0
Eran Rivlis <eran.rivlis@yahoo.com>
2025-02-24 23:44:34 +0200

    add code examples - html dom - tokens

---
+++ examples/code/html_dom.py
@@ -0,0 +1,61 @@
+import base64
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+import graphinate
+
+
+def load_html_from_url(url="https://www.google.com"):
+    response = requests.get(url)
+    return response.text
+
+
+def load_html(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+
+def html_dom_graph_model(html_content):
+    graph_model = graphinate.model(name="HTML DOM Graph")
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    def node_type(tag: Tag):
+        return tag.name.strip('[]')
+
+    def node_key(tag: Tag):
+        return str((tag.sourceline, tag.sourcepos)) if isinstance(tag, Tag) else base64.b64encode(
+            tag.encode()).decode()
+
+    def node_label(tag: Tag):
+        return str(tag)
+
+    @graph_model.node(node_type, key=node_key, label=node_label)
+    def html_node():
+        for tag in soup.descendants:
+            if tag.name is not None:
+                yield tag
+
+    @graph_model.edge()
+    def contains():
+        for tag in soup.descendants:
+            if tag.name is not None:
+                for child in tag.children:
+                    if child.name is not None:
+                        yield {
+                            'source': node_key(tag),
+                            'target': node_key(child)
+                        }
+
+    return graph_model
+
+
+if __name__ == '__main__':
+    html_content = load_html_from_url()
+    dom_model = html_dom_graph_model(html_content)
+    graphinate.materialize(
+        dom_model,
+        builder=graphinate.builders.GraphQLBuilder,
+        builder_output_handler=graphinate.graphql
+    )
+
---
+++ examples/code/tokens.py
@@ -0,0 +1,53 @@
+import itertools
+import operator
+import sys
+
+from pygments import lex
+from pygments.lexers import guess_lexer_for_filename
+
+import graphinate
+
+
+def load_file(file_path):
+    with open(file_path) as file:
+        return file.read()
+
+
+def tokenize_file(file_path):
+    content = load_file(file_path)
+    lexer = guess_lexer_for_filename(file_path, content)
+    return lex(content, lexer)
+
+
+def token_graph_model(file_path):
+    graph_model = graphinate.model(name="Token Graph")
+
+    def token_type(v):
+        return str(v[0]).replace('.', '_')
+
+    def token_key(v):
+        return f"{v[0]}-{v[1]}"
+
+    @graph_model.node(token_type, key=token_key)
+    def token():
+        yield from tokenize_file(file_path)
+
+    @graph_model.edge(source=operator.itemgetter(0), target=operator.itemgetter(1))
+    def edge():
+        yield from itertools.pairwise(token_key(t) for t in tokenize_file(file_path))
+
+    return graph_model
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python tokens.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    token_model = token_graph_model(file_path)
+    graphinate.materialize(
+        token_model,
+        builder=graphinate.builders.GraphQLBuilder,
+        builder_output_handler=graphinate.graphql
+    )
