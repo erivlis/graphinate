e28fa8ca47ffc137149acf375fe90a4bf8c27b4f
Eran Rivlis <eran.rivlis@yahoo.com>
2025-03-20 02:16:32 +0200

    refactor + updates

--- src/graphinate/renderers/graphql.py
+++ src/graphinate/renderers/graphql.py
@@ -29,7 +29,7 @@ def _openapi_schema(request: Request) -> ASGIApp:
     """
     schema_data = {
         'openapi': '3.0.0',
-        'info': {'title': 'Graphinate API', 'version': '0.6.0'},
+        'info': {'title': 'Graphinate API', 'version': '0.8.1'},
         'paths': {
             '/graphql': {'get': {'responses': {200: {'description': 'GraphQL'}}}},
             '/graphiql': {'get': {'responses': {200: {'description': 'GraphiQL UI.'}}}},
@@ -43,6 +43,45 @@ def _openapi_schema(request: Request) -> ASGIApp:
     return schema.OpenAPIResponse(request=request)
 
 
+def _graphql_app(graphql_schema: strawberry.Schema) -> strawberry.asgi.GraphQL:
+    graphql_schema.extensions.append(OpenTelemetryExtension)
+    graphql_app = GraphQL(graphql_schema, graphiql=True)
+    return graphql_app
+
+
+def _starlette_app(graphql_app: strawberry.asgi.GraphQL | None = None, port: int = DEFAULT_PORT, **kwargs) -> Starlette:
+    def open_url(endpoint):
+        webbrowser.open(f'http://localhost:{port}/{endpoint}')
+
+    @contextlib.asynccontextmanager
+    async def lifespan(app: Starlette):  # pragma: no cover
+        if kwargs.get('browse'):
+            open_url('viewer')
+        yield
+
+    app = Starlette(
+        lifespan=lifespan,
+        routes=routes()
+    )
+
+    from starlette_prometheus import PrometheusMiddleware, metrics
+    app.add_middleware(PrometheusMiddleware)
+    app.add_route("/metrics", metrics)
+
+    if graphql_app:
+        app.add_route(GRAPHQL_ROUTE_PATH, graphql_app)
+        app.add_websocket_route(GRAPHQL_ROUTE_PATH, graphql_app)
+        app.add_route("/schema", route=_openapi_schema, include_in_schema=False)
+        app.add_route("/openapi.json", route=_openapi_schema, include_in_schema=False)
+
+    async def redirect_to_viewer(request):
+        return RedirectResponse(url='/viewer')
+
+    app.add_route('/', redirect_to_viewer)
+
+    return app
+
+
 def server(graphql_schema: strawberry.Schema, port: int = DEFAULT_PORT, **kwargs):
     """
     Args:
@@ -51,36 +90,10 @@ def server(graphql_schema: strawberry.Schema, port: int = DEFAULT_PORT, **kwargs
 
     Returns:
     """
-    graphql_schema.extensions.append(OpenTelemetryExtension)
 
-    def open_url():
-        for app_name in ('viewer',):
-            webbrowser.open(f'http://localhost:{port}/{app_name}')
+    graphql_app = _graphql_app(graphql_schema)
 
-    @contextlib.asynccontextmanager
-    async def lifespan(app: Starlette):  # pragma: no cover
-        if kwargs.get('browse'):
-            open_url()
-        yield
-
-    graphql_app = GraphQL(graphql_schema, graphiql=True, graphql_ide='pathfinder')
-    app = Starlette(
-        lifespan=lifespan,
-        routes=routes()
-    )
-    app.add_route(GRAPHQL_ROUTE_PATH, graphql_app)
-    app.add_websocket_route(GRAPHQL_ROUTE_PATH, graphql_app)
-
-    from starlette_prometheus import PrometheusMiddleware, metrics
-    app.add_middleware(PrometheusMiddleware)
-    app.add_route("/metrics", metrics)
-    app.add_route("/schema", route=_openapi_schema, include_in_schema=False)
-    app.add_route("/openapi.json", route=_openapi_schema, include_in_schema=False)
-
-    async def redirect_to_viewer(request):
-        return RedirectResponse(url='/viewer')
-
-    app.add_route('/', redirect_to_viewer)
+    app = _starlette_app(graphql_app, port=port, **kwargs)
 
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=port)
