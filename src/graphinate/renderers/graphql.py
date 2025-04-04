a5c7a1d4171cafe5cb4d0194e6e2399b68422d4a
Eran Rivlis <eran.rivlis@yahoo.com>
2025-04-05 01:19:51 +0300

    fix: downgrade version from 0.8.6 to 0.8.5 in graphql.py and pyproject.toml

--- src/graphinate/renderers/graphql.py
+++ src/graphinate/renderers/graphql.py
@@ -29,7 +29,7 @@ def _openapi_schema(request: Request) -> ASGIApp:
     """
     schema_data = {
         'openapi': '3.0.0',
-        'info': {'title': 'Graphinate API', 'version': '0.8.6'},
+        'info': {'title': 'Graphinate API', 'version': '0.8.5'},
         'paths': {
             '/graphql': {'get': {'responses': {200: {'description': 'GraphQL'}}}},
             '/graphiql': {'get': {'responses': {200: {'description': 'GraphiQL UI.'}}}},
