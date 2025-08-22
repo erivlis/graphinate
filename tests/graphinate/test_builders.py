463f3d7cfccf5a317d01f235b805edb14e4881a2
Eran Rivlis <eran.rivlis@yahoo.com>
2025-08-22 22:46:43 +0300

    test: add unit tests for NetworkxBuilder._populate_nodes with various scenarios

--- tests/graphinate/test_builders.py
+++ tests/graphinate/test_builders.py
@@ -1,9 +1,13 @@
+from collections import Counter
+
 import networkx as nx
 import pytest
 
 import graphinate
 import graphinate.builders
 from graphinate import GraphType
+from graphinate.modeling import GraphModel, Multiplicity
+from graphinate.typing import UniverseNode
 
 graph_types = [
     (nx.Graph(), GraphType.Graph),
@@ -303,3 +307,209 @@ def test_graphql_builder__ast_model__graph_query(ast_graph_model, graphql_query)
     assert actual_graph['graph']['name'] == 'AST Graph'
     node_types_counts = {c['name']: c['value'] for c in actual_graph['graph']['nodeTypeCounts']}
     assert node_types_counts
+
+
+class DummyNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+class DummyNodeModel:
+    def __init__(self, uniqueness=True, label=None, type_='dummy', multiplicity=Multiplicity.ALL,
+                 parent_type=UniverseNode, generator=None):
+        self.uniqueness = uniqueness
+        self.label = label
+        self.type = type_
+        self.multiplicity = multiplicity
+        self.parent_type = parent_type
+        self.generator = generator or (lambda **kwargs: [])
+
+
+@pytest.fixture
+def empty_graph_model():
+    model = GraphModel(name="test")
+    model._node_models = {}
+    return model
+
+
+@pytest.fixture
+def builder_with_graph(empty_graph_model):
+    builder = graphinate.builders.NetworkxBuilder(empty_graph_model)
+    builder._graph = nx.Graph(name="test", node_types=Counter(), edge_types=Counter())
+    return builder
+
+
+def test_populate_nodes_adds_new_nodes_with_attributes(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n1', value=42)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    assert node_id in builder_with_graph._graph
+    attrs = builder_with_graph._graph.nodes[node_id]
+    assert attrs['label'] == node.key
+    assert attrs['type'] == 'dummynode'
+    assert attrs['value'] == [node.value]
+    assert attrs['magnitude'] == 1
+    assert attrs['lineage'] == [node.key]
+    assert 'created' in attrs
+
+
+def test_populate_nodes_adds_edges_for_non_universe_parent(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n1', value=1)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator, parent_type='not_universe')
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    parent_node_id = builder_with_graph._parent_node_id(node_type_absolute_id)
+    node_id = (node.key,)
+    assert (parent_node_id, node_id) in builder_with_graph._graph.edges
+
+
+def test_populate_nodes_handles_empty_generator(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+
+    def generator(**kwargs):
+        if False:
+            yield
+
+    node_model = DummyNodeModel(generator=generator)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    assert len(builder_with_graph._graph.nodes) == 0
+    assert len(builder_with_graph._graph.edges) == 0
+
+
+def test_populate_nodes_callable_label_assignment(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n1', value=7)
+
+    def generator(**kwargs):
+        yield node
+
+    def label_fn(value):
+        return f"label-{value}"
+
+    node_model = DummyNodeModel(generator=generator, label=label_fn)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    assert builder_with_graph._graph.nodes[node_id]['label'] == "label-7"
+
+
+def test_populate_nodes_handles_invalid_parent_node_id(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n1', value=1)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator, parent_type=UniverseNode)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    # Should not create any edge since parent_type is UniverseNode
+    assert len(builder_with_graph._graph.edges) == 0
+    assert node_id in builder_with_graph._graph
+
+
+def test_populate_nodes_adds_new_node(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n2', value=99)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    assert node_id in builder_with_graph._graph
+    assert builder_with_graph._graph.nodes[node_id]['value'] == [99]
+
+
+def test_populate_nodes_updates_existing_node(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n3', value=5)
+
+    def generator(**kwargs):
+        yield node
+        yield node
+
+    node_model = DummyNodeModel(generator=generator, multiplicity=Multiplicity.ALL)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    assert builder_with_graph._graph.nodes[node_id]['value'] == [5, 5]
+    assert builder_with_graph._graph.nodes[node_id]['magnitude'] == 2
+
+
+def test_populate_nodes_adds_edge_for_non_universe_parent(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n4', value=8)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator, parent_type='not_universe')
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    parent_node_id = builder_with_graph._parent_node_id(node_type_absolute_id)
+    node_id = (node.key,)
+    assert (parent_node_id, node_id) in builder_with_graph._graph.edges
+
+
+def test_populate_nodes_callable_label_returns_non_string(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+    node = DummyNode(key='n5', value=123)
+
+    def generator(**kwargs):
+        yield node
+
+    def label_fn(value):
+        return 999  # Non-string label
+
+    node_model = DummyNodeModel(generator=generator, label=label_fn)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    assert builder_with_graph._graph.nodes[node_id]['label'] == 999
+
+
+def test_populate_nodes_empty_generator(builder_with_graph):
+    node_type_absolute_id = ('parent', 'child')
+
+    def generator(**kwargs):
+        return
+        yield
+
+    node_model = DummyNodeModel(generator=generator)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    assert len(builder_with_graph._graph.nodes) == 0
+
+
+def test_populate_nodes_universe_node_parent_handling(builder_with_graph):
+    node_type_absolute_id = (UniverseNode, 'child')
+    node = DummyNode(key='n6', value=55)
+
+    def generator(**kwargs):
+        yield node
+
+    node_model = DummyNodeModel(generator=generator, parent_type=UniverseNode)
+    builder_with_graph.model._node_models = {node_type_absolute_id: [node_model]}
+    builder_with_graph._populate_nodes(node_type_absolute_id)
+    node_id = (node.key,)
+    # Lineage should be (node.key,) and no edge should be created
+    assert builder_with_graph._graph.nodes[node_id]['lineage'] == [node.key]
+    assert len(builder_with_graph._graph.edges) == 0
