987820a0ca0ca349d814d5fd8451d16dd5dcc4c8
Eran Rivlis <eran.rivlis@yahoo.com>
2025-11-17 00:57:49 +0200

    updates project to 0.10.0

--- src/graphinate/renderers/graphql.py
+++ src/graphinate/renderers/graphql.py
@@ -28,7 +28,7 @@ def _openapi_schema(request: Request) -> ASGIApp:
     """
     schema_data = {
         'openapi': '3.0.0',
-        'info': {'title': 'Graphinate API', 'version': '0.9.1'},
+        'info': {'title': 'Graphinate API', 'version': '0.10.0'},
         'paths': {
             '/graphql': {'get': {'responses': {200: {'description': 'GraphQL'}}}},
             '/graphiql': {'get': {'responses': {200: {'description': 'GraphiQL UI.'}}}},
